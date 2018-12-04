bca4375083247dbabf9b7b86414568a70d99632c
Joel Ahlgren <joel@ahlgren.io>
2018-12-04 13:50:00 +0100

    Day 4, part 1 solved.

--- AdventOfCode2018.Tests/Problems/Problem4Tests.cs
+++ AdventOfCode2018.Tests/Problems/Problem4Tests.cs
@@ -1,16 +1,73 @@
 ﻿namespace AdventOfCode2018.Tests.Problems
 {
     using AdventOfCode2018.Problems;
-    
+
     using NUnit.Framework;
 
     [TestFixture]
     public class Problem4Tests
     {
-        [Test]
-        public void Test()
+        private static readonly string[] TestInput = 
         {
+            "[1518-11-01 00:00] Guard #10 begins shift",
+            "[1518-11-01 00:05] falls asleep",
+            "[1518-11-01 00:25] wakes up",
+            "[1518-11-01 00:30] falls asleep",
+            "[1518-11-01 00:55] wakes up",
+            "[1518-11-01 23:58] Guard #99 begins shift",
+            "[1518-11-02 00:40] falls asleep",
+            "[1518-11-02 00:50] wakes up",
+            "[1518-11-03 00:05] Guard #10 begins shift",
+            "[1518-11-03 00:24] falls asleep",
+            "[1518-11-03 00:29] wakes up",
+            "[1518-11-04 00:02] Guard #99 begins shift",
+            "[1518-11-04 00:36] falls asleep",
+            "[1518-11-04 00:46] wakes up",
+            "[1518-11-05 00:03] Guard #99 begins shift",
+            "[1518-11-05 00:45] falls asleep",
+            "[1518-11-05 00:55] wakes up"
+        };
 
+        private static readonly string[] TestInputJumbled =
+        {
+            "[1518-11-03 00:29] wakes up",
+            "[1518-11-04 00:02] Guard #99 begins shift",
+            "[1518-11-04 00:36] falls asleep",
+            "[1518-11-04 00:46] wakes up",
+            "[1518-11-05 00:03] Guard #99 begins shift",
+            "[1518-11-05 00:45] falls asleep",
+            "[1518-11-05 00:55] wakes up",
+            "[1518-11-01 00:00] Guard #10 begins shift",
+            "[1518-11-01 00:55] wakes up",
+            "[1518-11-01 23:58] Guard #99 begins shift",
+            "[1518-11-02 00:40] falls asleep",
+            "[1518-11-01 00:25] wakes up",
+            "[1518-11-01 00:30] falls asleep",
+            "[1518-11-01 00:05] falls asleep",
+            "[1518-11-02 00:50] wakes up",
+            "[1518-11-03 00:05] Guard #10 begins shift",
+            "[1518-11-03 00:24] falls asleep"
+
+        };
+
+        [Test]
+        public void TestLogSorting()
+        {
+            Assert.AreEqual(Problem4.ParseInput(TestInputJumbled), Problem4.ParseInput(TestInput));
+        }
+
+        [Test]
+        public void Strategy1Test()
+        {
+            var sortedLogs = Problem4.ParseInput(TestInput);
+            Assert.AreEqual(240, Problem4.Strategy1(sortedLogs));
+        }
+
+        [Test]
+        public void Strategy2Test()
+        {
+            var sortedLogs = Problem4.ParseInput(TestInput);
+            Assert.AreEqual(4455, Problem4.Strategy2(sortedLogs));
         }
     }
 }
--- AdventOfCode2018/Problems/Problem4.cs
+++ AdventOfCode2018/Problems/Problem4.cs
@@ -1,14 +1,134 @@
 ﻿namespace AdventOfCode2018.Problems
 {
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text.RegularExpressions;
+
     public class Problem4 : Problem
     {
         public Problem4() : base(4)
         {
         }
 
+        public static List<Log> ParseInput(IList<string> input)
+        {
+            var output = input.Select(message => new Log(message)).ToList();
+            output.Sort();
+
+            return output;
+        }
+
+        public static int Strategy1(IList<Log> sortedLogs)
+        {
+            // (Guard : Total Slept minutes)
+            var totalSleepLog = new Dictionary<int, int>();
+
+            // (Guard : (Minute : TimesSlept))
+            var minuteSleepLog = new Dictionary<int, Dictionary<int, int>>();
+
+            var currentId = -1;
+            var fellAsleep = -1;
+
+            foreach (var log in sortedLogs)
+            {
+                if (log.Message.Contains("begins shift"))
+                {
+                    currentId = Convert.ToInt32(Regex.Match(log.Message, @"#(\d+) ").Groups[1].Value);
+                    continue;
+                }
+
+                if (log.Message.Contains("falls asleep"))
+                {
+                    fellAsleep = log.TimeStamp.Minute;
+                    continue;
+                }
+
+                if (log.Message.Contains("wakes up"))
+                {
+                    var wokeUp = log.TimeStamp.Minute;
+
+                    if (!minuteSleepLog.ContainsKey(currentId))
+                    {
+                        minuteSleepLog.Add(currentId, new Dictionary<int, int>());
+                    }
+
+                    for (var t = fellAsleep; t < wokeUp; t++)
+                    {
+                        if (!minuteSleepLog[currentId].ContainsKey(t))
+                        {
+                            minuteSleepLog[currentId].Add(t, 0);
+                        }
+
+                        minuteSleepLog[currentId][t]++;
+                    }
+
+                    if (!totalSleepLog.ContainsKey(currentId))
+                    {
+                        totalSleepLog.Add(currentId, 0);
+                    }
+
+                    totalSleepLog[currentId] += wokeUp - fellAsleep;
+                }
+            }
+
+            var biggestSleeper = totalSleepLog.Aggregate((l, r) => l.Value > r.Value ? l : r).Key;
+            var mostSleptMinute = minuteSleepLog[biggestSleeper].Aggregate((l, r) => l.Value > r.Value ? l : r).Key;
+
+
+            return biggestSleeper * mostSleptMinute;
+        }
+
+        public static int Strategy2(IList<Log> sortedLogs)
+        {
+
+
+            return 0;
+        }
+
         public override string Answer()
         {
-            throw new System.NotImplementedException();
+            var sortedLogs = ParseInput(Input);
+            return $"Strategy 1 gives: {Strategy1(sortedLogs)}";
+        }
+    }
+
+    public class Log : IComparable
+    {
+        public DateTime TimeStamp { get; }
+
+        public string Message { get; }
+
+        public Log(string input)
+        {
+            var matches = Regex.Match(input, @"\[(\d\d\d\d)-(\d\d)-(\d\d) (\d\d):(\d\d)] (.*)").Groups;
+            TimeStamp = new DateTime(
+                Convert.ToInt32(matches[1].Value),  // Year
+                Convert.ToInt32(matches[2].Value),  // Month
+                Convert.ToInt32(matches[3].Value),  // Day
+                Convert.ToInt32(matches[4].Value),  // Hour
+                Convert.ToInt32(matches[5].Value),  // Minute
+                0                                   // Second
+                );
+
+            Message = matches[6].Value;
+        }
+
+        public override string ToString()
+        {
+            return $"{TimeStamp} | {Message}";
+        }
+
+        public override bool Equals(object obj)
+        {
+            var other = (Log)obj;
+            return other.TimeStamp.Equals(TimeStamp) && other.Message.Equals(Message);
+        }
+
+        public int CompareTo(object obj)
+        {
+            var other = (Log) obj;
+            return TimeStamp.CompareTo(other.TimeStamp);
         }
     }
 }
