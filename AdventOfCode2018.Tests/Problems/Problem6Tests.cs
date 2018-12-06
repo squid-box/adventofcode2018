1023da282c507930dda11a8e1054aaacf97dee4d
Joel Ahlgren <joel@ahlgren.io>
2018-12-06 09:33:54 +0100

    Day 6: Barely started.

---
+++ AdventOfCode2018.Tests/Problems/Problem6Tests.cs
@@ -0,0 +1,34 @@
+﻿using AdventOfCode2018.Problems;
+
+namespace AdventOfCode2018.Tests.Problems
+{
+    using NUnit.Framework;
+
+    [TestFixture]
+    public class Problem6Tests
+    {
+        private static readonly string[] TestInput =
+        {
+            "1, 1",
+            "1, 6",
+            "8, 3",
+            "3, 4",
+            "5, 5",
+            "8, 9"
+        };
+
+        [Test]
+        public void ParseInputTest()
+        {
+            var coordinates = Problem6.ParseInput(TestInput);
+            Assert.AreEqual(6, coordinates.Count);
+        }
+
+        [Test]
+        public void FindLargestAreaTest()
+        {
+            var coordinates = Problem6.ParseInput(TestInput);
+            Assert.AreEqual(17, Problem6.FindLargestArea(coordinates));
+        }
+    }
+}
--- AdventOfCode2018/Problems/Problem6.cs
+++ AdventOfCode2018/Problems/Problem6.cs
@@ -1,11 +1,32 @@
 ﻿namespace AdventOfCode2018.Problems
 {
     using System;
+    using System.Collections.Generic;
+
+    using Utils;
 
     public class Problem6 : Problem
     {
         public Problem6() : base(6)
         {
+        }
+
+        public static IList<Coordinate> ParseInput(string[] input)
+        {
+            var coordinates = new List<Coordinate>();
+
+            foreach (var line in input)
+            {
+                var tmp = line.Split(',');
+                coordinates.Add(new Coordinate(Convert.ToInt32(tmp[0]), Convert.ToInt32(tmp[1])));
+            }
+
+            return coordinates;
+        }
+
+        public static int FindLargestArea(IList<Coordinate> coordinates)
+        {
+            return 0;
         }
 
         public override string Answer()
