3aeabf519a497995ded88aefdb7cc229a286d92b
Joel Ahlgren <joel@ahlgren.io>
2018-12-08 13:46:01 +0100

    Day 8 started.

---
+++ AdventOfCode2018.Tests/Problems/Problem8Tests.cs
@@ -0,0 +1,21 @@
+﻿namespace AdventOfCode2018.Tests.Problems
+{
+    using AdventOfCode2018.Problems;
+
+    using NUnit.Framework;
+
+    [TestFixture]
+    public class Problem8Tests
+    {
+	    private static readonly string TestInput = "2 3 0 3 10 11 12 1 1 0 1 99 2 1 1 2";
+
+        [Test]
+        public void ParseInputTest()
+        {
+	        var rootNode = Problem8.ParseInput(TestInput);
+
+			Assert.AreEqual(2, rootNode.Children.Count);
+	        Assert.AreEqual(3, rootNode.Metadata.Count);
+		}
+    }
+}
--- AdventOfCode2018/Problems/Problem8.cs
+++ AdventOfCode2018/Problems/Problem8.cs
@@ -1,4 +1,6 @@
-﻿namespace AdventOfCode2018.Problems
+﻿using System.Collections.Generic;
+
+namespace AdventOfCode2018.Problems
 {
     using System;
 
@@ -8,9 +10,35 @@
         {
         }
 
+	    public static Node ParseInput(string input)
+	    {
+		    var split = input.Split(' ');
+
+		    for (var i = 0; i < split.Length-1; i++)
+		    {
+			    var numberOfChildren = Convert.ToInt32(split[i]);
+			    var numberOfDataEntries = Convert.ToInt32(split[i + 1]);
+		    }
+
+		    return null;
+	    }
+
         public override string Answer()
         {
             throw new NotImplementedException();
         }
     }
+
+	public class Node
+	{
+		public List<Node> Children { get; }
+
+		public List<int> Metadata { get; }
+
+		public Node()
+		{
+			Children = new List<Node>();
+			Metadata = new List<int>();
+		}
+	}
 }
