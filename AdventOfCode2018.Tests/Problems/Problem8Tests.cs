e2395be41e7c088b0bfcfbb604751ddff9ad7b4e
Joel Ahlgren <joel@ahlgren.io>
2018-12-08 23:03:58 +0100

    Day 8 complete!

--- AdventOfCode2018.Tests/Problems/Problem8Tests.cs
+++ AdventOfCode2018.Tests/Problems/Problem8Tests.cs
@@ -17,5 +17,21 @@
 			Assert.AreEqual(2, rootNode.Children.Count);
 	        Assert.AreEqual(3, rootNode.Metadata.Count);
 		}
+
+        [Test]
+        public void FindMetadataSumTest()
+        {
+            var rootNode = Problem8.ParseInput(TestInput);
+
+            Assert.AreEqual(138, Problem8.FindMetadataSum(rootNode));
+        }
+
+        [Test]
+        public void FindValueTest()
+        {
+            var rootNode = Problem8.ParseInput(TestInput);
+
+            Assert.AreEqual(66, Problem8.FindValue(rootNode));
+        }
     }
 }
--- AdventOfCode2018/Problems/Problem8.cs
+++ AdventOfCode2018/Problems/Problem8.cs
@@ -1,31 +1,77 @@
-﻿using System.Collections.Generic;
-
-namespace AdventOfCode2018.Problems
+﻿namespace AdventOfCode2018.Problems
 {
     using System;
+    using System.Collections.Generic;
+    using System.Linq;
 
     public class Problem8 : Problem
     {
         public Problem8() : base(8)
         {
         }
-
 	    public static Node ParseInput(string input)
 	    {
-		    var split = input.Split(' ');
+		    var split = input.Split(' ').Select(i => Convert.ToInt32(i)).ToList();
+	        var index = 0;
 
-		    for (var i = 0; i < split.Length-1; i++)
-		    {
-			    var numberOfChildren = Convert.ToInt32(split[i]);
-			    var numberOfDataEntries = Convert.ToInt32(split[i + 1]);
-		    }
-
-		    return null;
+	        return ParseNode(split, ref index);
 	    }
+
+        private static Node ParseNode(IList<int> numbers, ref int index)
+        {
+            var node = new Node();
+            var children = numbers[index++];
+            var metadata = numbers[index++];
+
+            for (var child = 0; child < children; child++)
+            {
+                node.Children.Add(ParseNode(numbers, ref index));
+            }
+
+            for (var data = 0; data < metadata; data++)
+            {
+                node.Metadata.Add(numbers[index++]);
+            }
+
+            return node;
+        }
+
+        public static int FindMetadataSum(Node node)
+        {
+            var sum = node.Metadata.Sum();
+
+            foreach (var child in node.Children)
+            {
+                sum += FindMetadataSum(child);
+            }
+
+            return sum;
+        }
+
+        public static int FindValue(Node node)
+        {
+            if (node.Children.Count == 0)
+            {
+                return node.Metadata.Sum();
+            }
+
+            var value = 0;
+
+            foreach (var data in node.Metadata)
+            {
+                if (data <= node.Children.Count)
+                {
+                    value += FindValue(node.Children[data-1]);
+                }
+            }
+
+            return value;
+        }
 
         public override string Answer()
         {
-            throw new NotImplementedException();
+            var rootNode = ParseInput(Input[0]);
+            return $"The sum of all metadata entries is {FindMetadataSum(rootNode)}, the value of the root node is {FindValue(rootNode)}.";
         }
     }
 
